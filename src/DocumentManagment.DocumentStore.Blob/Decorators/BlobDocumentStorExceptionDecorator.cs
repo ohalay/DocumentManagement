29f0d9c27a9040c74b55cf3b41a676452d7256df
Oleh Halay <ohalay@intellias.com>
2020-04-17 11:16:42 +0300

    add storage exception decorator (draft)

---
+++ src/DocumentManagment.DocumentStore.Blob/Decorators/BlobDocumentStorExceptionDecorator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Storage.Blobs.Models;
+using DocumentManagement.Core;
+using DocumentManagement.Core.Component;
+using DocumentManagement.Core.Domain;
+using Polly;
+
+namespace DocumentManagement.DocumentStore.Blob.Decorators
+{
+    /// <summary>
+    /// Retry blob document store decorator.
+    /// </summary>
+    internal class BlobDocumentStorExceptionDecorator : IDocumentStore
+    {
+        private readonly IDocumentStore docuemtnStore;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlobDocumentStorExceptionDecorator"/> class.
+        /// </summary>
+        /// <param name="docuemtnStore">Document store.</param>
+        public BlobDocumentStorExceptionDecorator(IDocumentStore docuemtnStore)
+        {
+            this.docuemtnStore = docuemtnStore;
+        }
+
+        public Task<OperationResult> DeleteAsync(string name)
+            => SafeExecuteAsync(() => docuemtnStore.DeleteAsync(name));
+
+        public Task<OperationResult<IReadOnlyCollection<DocumentEntity>>> GetAllAsync()
+            => SafeExecuteAsync(() => docuemtnStore.GetAllAsync());
+
+        public Task<OperationResult> ReorderAsync(params DocumentEntity[] documents)
+            => SafeExecuteAsync(() => docuemtnStore.ReorderAsync(documents));
+
+        public Task<OperationResult> UploadAsync(string name, Stream stream)
+             => SafeExecuteAsync(() => docuemtnStore.UploadAsync(name, stream));
+
+        private async Task<TResponse> SafeExecuteAsync<TResponse>(Func<Task<TResponse>> func)
+            where TResponse : OperationResult
+        {
+            try
+            {
+                return await func();
+            }
+            catch (RequestFailedException e) when (e.ErrorCode == BlobErrorCode.BlobNotFound.ToString())
+            {
+                return new OperationResult($"Document name is not found.") as TResponse;
+            }
+            catch (RequestFailedException ex)
+            {
+                return new OperationResult(ex.Message) as TResponse;
+            }
+        }
+    }
+}
